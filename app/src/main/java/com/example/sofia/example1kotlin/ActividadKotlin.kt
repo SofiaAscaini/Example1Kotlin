276f53b234f6d8c7736c4a5ef3689115c95d9fe5
DESKTOP-JLTDLUF\sofia <ascainisofia@gmail.com>
2018-05-23 20:09:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sofia/example1kotlin/ActividadKotlin.kt
@@ -0,0 +1,13 @@
+package com.example.sofia.example1kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActividadKotlin : AppCompatActivity() {
+
+//onCreate método inicial.
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad_kotlin)
+    }
+}
